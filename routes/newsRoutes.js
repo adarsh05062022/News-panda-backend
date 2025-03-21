0cb005cd958808f6536b223fbe978dbf4ed9cafc
Adarsh Dubey <anmold652@gmail.com>
2025-03-22 00:36:53 +0530

    backend service

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -3,6 +3,11 @@ import { getNews, fetchNews } from '../controllers/newsControllers.js';
 
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+    res.send('Welcome to the News API');
+  });
+
 router.get('/fetch-news', fetchNews);
 router.get('/news', getNews);
 
